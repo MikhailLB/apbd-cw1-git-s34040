d8f5564b049fb2c05525bcc4441c6ebd869016a2
Mikhail <s34040@pjwstk.edu.pl>
2026-03-18 19:51:06 +0100

    User(Add ORM iterface for user entity)

---
+++ ConsoleApp2/internal/user/userModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp2.Internal.Users;
+
+public record User(int Id, string Name, string Email);
+
+public interface IUserStorage
+{
+    User GetUser(int id);
+    
+    void UpdateUser(User user);
+
+    void PartiallyUpdateUser(int id, object updates);
+}
